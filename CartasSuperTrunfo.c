7dd5a858df8af6ed15e1b5de1cf7a7a12400a8e6
pdavila054-rgb <pdavila054@gmail.com>
2026-08-12 14:01:06 +0000

    Completei o desafio iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,58 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char País[40];
+    char Estado[40];
+    char Código_da_carta[20];
+    char Nome_da_cidade[40];
+    int População;
+    float Área_em_km2;
+    float PIB;
+    int Número_de_pontos_turisticos;
 
   // Área para entrada de dados
+    printf("Olá, seja bem vindo ao Super trunfo-Países!\n");
 
+    printf("O Super Trunfo é um jogo de cartas onde cada carta possui atributos diferentes.\n");
+
+    printf("Para o jogo começarserá necesario que você cadastre uma carta com os atributos do país.\n   ");
+
+    printf("informe o nome do país: ");
+    scanf("%s", País);
+
+    printf("Agora, inoforme o nome de um estado desse país: ");
+    scanf("%s", Estado);
+
+    printf("Agora, informe o código da carta: ");
+    scanf("%s", Código_da_carta);
+
+    printf("Agora, informe o nome de uma cidade: ");
+    scanf("%s", Nome_da_cidade);
+
+    printf("Agora, informe a população desta cidade: ");
+    scanf("%d", &População);
+
+    printf("Agora, informe a área em km² desta cidade: ");
+    scanf("%f", &Área_em_km2);
+
+    printf("Agora, informe o PIB desta cidade: ");
+    scanf("%f", &PIB);
+
+    printf("Agora, informe o número de pontos turísticos que esta cidade possui: ");
+    scanf("%d", &Número_de_pontos_turisticos);
+
+    printf("Carta cadastrada com sucesso!\n");
+    printf("Espere um momento, a sua carta esta sendo construida...\n");
   // Área para exibição dos dados da cidade
+
+  printf("País: %s\n", País);
+    printf("Estado: %s\n", Estado);
+    printf("Código da carta: %s\n", Código_da_carta);
+    printf("Nome da cidade: %s\n", Nome_da_cidade);
+    printf("População: %d\n", População);
+    printf("Área em km²: %.2f\n", Área_em_km2);
+    printf("PIB: %.2f\n", PIB);
+    printf("Número de pontos turísticos: %d\n", Número_de_pontos_turisticos);
 
 return 0;
 } 
